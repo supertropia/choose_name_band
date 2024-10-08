b183032a9de9eccf58cfa8f5eb4f6dc27832c4bd
supertropia . LSc <77046352+supertropia@users.noreply.github.com>
2024-10-08 09:21:12 -0300

    Create main.py
    choose the name of a rock band

---
+++ main.py
@@ -0,0 +1,6 @@
+print("Welcome to the band name generator\n")
+name= input("What's name of the city you grew up in?.\n")
+city=input("What´s your pet´s name?\n")
+
+name_band: str = name + city
+print("Your band name could be " + name_band )
